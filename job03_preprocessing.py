56e9d761d2a912bb31fc5e940cfd197bea4a2033
chipmunk-tail <j1997m@naver.com>
2025-02-04 11:57:36 +0900

    update job03_preprocessing.py

--- job03_preprocessing.py
+++ job03_preprocessing.py
@@ -2,5 +2,24 @@
 
 import pandas as pd
 from konlpy.tag import Okt
+import re
 
+from job01_crawling import review
+
+df = pd.read_csv('./crawling_data/reviews_kinolights.csv')
+df. info()
+
+df_stopwords = pd.read_csv('./format_files/stopwords_kor.csv')
+stopwords = list(df_stopwords['stopword_kor'])
+# print(stopwords)
+
+okt = Okt()
+print(df.titles[0])
+print(df.reviews[0])
+
+review = re.sub('[^가-힣]', ' ', review)
+print(review)
+
+tokened_review = okt.pos(review, stem = True)
+print(tokened_review)
 
