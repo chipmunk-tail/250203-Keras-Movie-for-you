074896246ed0cb8677f3fd12dde54bddcd1bc20f
chipmunk-tail <j1997m@naver.com>
2025-02-07 15:08:07 +0900

    job06 change

--- job06_recommendation.py
+++ job06_recommendation.py
@@ -3,6 +3,7 @@
 # 5. 데이터 벡터화 및 pickle로 저장
 
 import pandas as pd
+from scipy.stats import cosine
 from sklearn.metrics.pairwise import linear_kernel
 from scipy.io import mmread
 import pickle
@@ -25,13 +26,41 @@ with open('./models/tfidf.pickle', 'rb') as f:
     Tfidf = pickle.load(f)
 
 
-# 영화 index 이용
-ref_idx = 349
-print('title', df_reviews.iloc[ref_idx, 0])
-cosine_sim = linear_kernel(Tfidf_matrix[ref_idx], Tfidf_matrix)
-print(cosine_sim[0])
-print(len(cosine_sim[0]))
-recommendations = getRecommendation(cosine_sim)
-print(recommendations)
+# # 영화 index 이용
+# ref_idx = 349
+# print('title', df_reviews.iloc[ref_idx, 0])
+# cosine_sim = linear_kernel(Tfidf_matrix[ref_idx], Tfidf_matrix)
+# print(cosine_sim[0])
+# print(len(cosine_sim[0]))
+# recommendations = getRecommendation(cosine_sim)
+# print(recommendations)
+
+embedding_model = Word2Vec.load('./models/word2vec_movie_review.model')
+keyword = '엄마'
+
+if keyword in list(embedding_model.wv.index_to_key):
+    sim_word = embedding_model.wv.most_similar(keyword, topn = 10)
+    words = [keyword]
+
+    for word, _ in sim_word:
+        words.append(word)
+    print(words)
+else:
+    print('not in')
+
+sentence =[]
+count = 10
+for word in words:
+    sentence = sentence + [word] * count
+    count -= 1
+
+sentence = ' '.join(sentence)
+print(sentence)
+
+sentence_vec = Tfidf.trasnsform([sentence])
+cosine_sim = linear_kernel(sentence_vec, Tfidf_matrix)
+recommendation = getRecommendation(cosine_sim)
+recommendation = '\n'.join(list(recommendation))
+
 
 
